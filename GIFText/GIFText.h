20c02f14a49394cd0f93a38852dddb13f126570e
Eric Young <ericyoung@gmail.com>
2016-02-23 11:09:39 -0500

    Initial Commit

---
+++ GIFText/GIFText.h
@@ -0,0 +1,19 @@
+//
+//  GIFText.h
+//  GIFText
+//
+//  Created by Eric Young on 2/23/16.
+//  Copyright © 2016 gifit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GIFText.
+FOUNDATION_EXPORT double GIFTextVersionNumber;
+
+//! Project version string for GIFText.
+FOUNDATION_EXPORT const unsigned char GIFTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GIFText/PublicHeader.h>
+
+
